6f36a66d9695d9445504abf26c688f27d8431a7d
dzh <dzh21@tut.by>
2014-10-21 22:17:21 +0300

    ticket:1 func test for admni Persons objects

--- ft/test.py
+++ ft/test.py
@@ -5,12 +5,14 @@ from django.test import LiveServerTestCase
 
 
 class AdminTest(LiveServerTestCase):
+    fixtures = ['initial_data.json']
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        self.browser.quit()
+        self.browser.close()
 
     def test_admin_site(self):
         self.browser.get('http://127.0.0.1:8081' + '/admin/')
@@ -21,6 +23,11 @@ class AdminTest(LiveServerTestCase):
         username_field.send_keys('admin')
         password_field = self.browser.find_element_by_name('password')
         password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
 
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Site administration', body.text)
+
+        persons_links = self.browser.find_elements_by_link_text('Persons')
+        self.assertEquals(len(persons_links), 2)
+
