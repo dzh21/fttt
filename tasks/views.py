58d962a5e7581b42ee42190da99731e911b93a89
dzh <dzh21@tut.by>
2014-10-23 11:44:55 +0300

    ticket:1 remove dir

--- tasks/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from tasks.models import Person
-
-
-def index(request):
-    context = {'persons': Person.objects.all()}
-    return render(request, "home.html", context)
